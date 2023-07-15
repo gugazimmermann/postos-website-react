7e5f6f26571dad53ec0db3aa119d43bd96a2b8b3
Guga Zimmermann <gugazimmermann@gmail.com>
2023-07-15 16:20:43 -0300

    start register form

--- src/register/RegisterForm.js
+++ src/register/RegisterForm.js
@@ -1,67 +1,176 @@
-import { Input, Label } from '../components';
+import { useState } from 'react';
+import { isValidCNPJ, isValidEmail, masks } from '../helpers';
+import { Alert, Button, Input, Label } from '../components';
+
+const Row = ({ children }) => <div className='mb-4 md:mb-0'>{children}</div>;
 
 const RegisterForm = () => {
-  const Row = ({ children }) => {
-    return <div className='mb-4 md:mb-0'>{children}</div>;
+  const [alert, setAlert] = useState();
+  const [loading, setLoading] = useState(false);
+  const [document, setDocument] = useState('');
+  const [name, setName] = useState('');
+  const [code, setCode] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [address, setAddress] = useState('');
+  const [city, setCity] = useState('');
+  const [state, setState] = useState('');
+
+  const resetForm = () => {
+    setDocument('');
+    setName('');
+    setCode('');
+    setEmail('');
+    setPhone('');
+    setAddress('');
+    setCity('');
+    setState('');
   };
 
-  const Button = () => {
-    return (
-      <div className='w-full text-center'>
-        <button
-          type='submit'
-          className='w-2/3 px-5 py-3 text-base font-medium text-center text-slate-50 bg-amber-500 rounded-lg hover:bg-amber-600'
-        >
-          Registrar
-        </button>
-      </div>
-    );
+  const validate = (data) => {
+    if (
+      !data.document ||
+      !data.name ||
+      !data.code ||
+      !data.email ||
+      !data.phone ||
+      !data.address ||
+      !data.city ||
+      !data.state
+    )
+      return 'Todos os campos são obrigatórios!';
+    if (!isValidCNPJ(data.document)) return 'CNPJ inválido!';
+    if (!isValidEmail(data.email)) return 'Email inválido!';
+    if (data?.name?.length < 3) return 'Nome deve ter mínimo de 3 caracteres!';
+    if (data?.code?.length < 3) return 'Código deve ter mínimo de 3 caracteres!';
+    return false;
+  };
+
+  const handleSubmit = async (e) => {
+    setAlert();
+    e.preventDefault();
+    setLoading(true);
+    const data = {
+      document,
+      name,
+      code,
+      email,
+      phone,
+      address,
+      city,
+      state,
+    };
+    const validationError = validate(data);
+    if (validationError) {
+      setAlert(validationError);
+      setLoading(false);
+      return;
+    }
+    setTimeout(() => {
+      console.log();
+      resetForm();
+      setLoading(false);
+    }, 5000);
   };
 
   return (
-    <form className='mt-8 space-y-6'>
-      <div className='md:grid md:grid-cols-2 md:gap-4'>
-        <Row>
-          <Label htmlFor='document' text='CNPJ' />
-          <Input name='document' />
-        </Row>
-        <Row>
-          <Label htmlFor='name' text='Nome' />
-          <Input name='name' />
-        </Row>
-      </div>
-      <div className='md:grid md:grid-cols-3 md:gap-4'>
-        <Row>
-          <Label htmlFor='code' text='Código' />
-          <Input name='code' />
-        </Row>
-        <Row>
-          <Label htmlFor='email' text='Email' />
-          <Input type='email' name='email' />
-        </Row>
-        <Row>
-          <Label htmlFor='phone' text='Telefone' />
-          <Input name='phone' />
-        </Row>
-      </div>
-      <div className='md:grid md:grid-cols-2 md:gap-4'>
-        <Row>
-          <Label htmlFor='address' text='Endedreço' />
-          <Input name='address' />
-        </Row>
-        <div className='grid grid-cols-2 gap-4'>
+    <>
+      {alert && <Alert type='error' text={alert} />}
+      <form onSubmit={(e) => handleSubmit(e)} className='mt-8 space-y-6'>
+        <div className='md:grid md:grid-cols-2 md:gap-4'>
           <Row>
-            <Label htmlFor='city' text='Cidade' />
-            <Input name='city' />
+            <Label htmlFor='document' text='CNPJ' />
+            <Input
+              loading={loading}
+              value={masks.cnpj(document)}
+              editValue={(e) => setDocument(masks.cnpj(e.target.value))}
+              required={true}
+              name='document'
+            />
           </Row>
           <Row>
-            <Label htmlFor='state' text='Estado' />
-            <Input name='state' />
+            <Label htmlFor='name' text='Nome' />
+            <Input
+              loading={loading}
+              value={name}
+              editValue={(e) => setName(e.target.value)}
+              required={true}
+              name='name'
+            />
           </Row>
         </div>
-      </div>
-      <Button />
-    </form>
+        <div className='md:grid md:grid-cols-3 md:gap-4'>
+          <Row>
+            <Label htmlFor='code' text='Código' />
+            <Input
+              loading={loading}
+              value={masks.code(code)}
+              editValue={(e) => setCode(masks.code(e.target.value))}
+              required={true}
+              name='code'
+            />
+          </Row>
+          <Row>
+            <Label htmlFor='email' text='Email' />
+            <Input
+              loading={loading}
+              value={email}
+              editValue={(e) => setEmail(e.target.value)}
+              required={true}
+              type='email'
+              name='email'
+            />
+          </Row>
+          <Row>
+            <Label htmlFor='phone' text='Telefone' />
+            <Input
+              loading={loading}
+              value={masks.phone(phone)}
+              editValue={(e) => setPhone(masks.phone(e.target.value))}
+              required={true}
+              name='phone'
+            />
+          </Row>
+        </div>
+        <div className='md:grid md:grid-cols-2 md:gap-4'>
+          <Row>
+            <Label htmlFor='address' text='Endedreço' />
+            <Input
+              loading={loading}
+              value={address}
+              editValue={(e) => setAddress(e.target.value)}
+              required={true}
+              name='address'
+            />
+          </Row>
+          <div className='grid grid-cols-2 gap-4'>
+            <Row>
+              <Label htmlFor='city' text='Cidade' />
+              <Input
+                loading={loading}
+                value={city}
+                editValue={(e) => setCity(e.target.value)}
+                required={true}
+                name='city'
+              />
+            </Row>
+            <Row>
+              <Label htmlFor='state' text='Estado' />
+              <Input
+                loading={loading}
+                value={state}
+                editValue={(e) => setState(e.target.value)}
+                required={true}
+                name='state'
+              />
+            </Row>
+          </div>
+        </div>
+        <div className='w-full text-center'>
+          <Button type='submit' loading={loading} text='Registrar' />
+        </div>
+      </form>
+    </>
   );
 };
 
