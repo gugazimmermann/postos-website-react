ba82eece98c140c0763b8df966fc1d68b44e8bb7
Guga Zimmermann <gugazimmermann@gmail.com>
2023-07-15 14:21:28 -0300

    add new components

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,8 @@
+export { default as Card } from './card/Card';
 export { default as Drawer } from './nav/Drawer';
 export { default as Footer } from './footer/Footer';
 export { default as Header } from './header/Header';
 export { default as Main } from './main/Main';
 export { default as NavItems } from './nav/NavItems';
+export { default as Input } from './form/Input';
+export { default as Label } from './form/Label';
