bbc7fa4d0570ff673a4540243e14076110b93e2d
Guga Zimmermann <gugazimmermann@gmail.com>
2023-07-15 15:16:27 -0300

    fix card space

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,6 +1,6 @@
 const Card = ({ title, children }) => {
   return (
-    <div className='w-full md:w-2/3 p-6 space-y-4 md:space-y-8 bg-white rounded-lg shadow-xl'>
+    <div className='w-full md:w-2/3 p-6 space-y-4 bg-white rounded-lg shadow-xl'>
       <h2 className='text-2xl font-bold text-slate-800 text-center md:text-left'>{title}</h2>
       {children}
     </div>
