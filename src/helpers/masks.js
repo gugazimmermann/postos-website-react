0c6b484d1c9ae43cb9c15e39600209454596c977
Guga Zimmermann <gugazimmermann@gmail.com>
2023-07-15 16:20:17 -0300

    add masks

---
+++ src/helpers/masks.js
@@ -0,0 +1,22 @@
+const code = (value) => value.replace(/[^a-zA-Z0-9_]/g, '')?.toLocaleLowerCase();
+
+const cnpj = (value) => {
+  return value
+    .replace(/\D+/g, '')
+    .replace(/(\d{2})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d)/, '$1/$2')
+    .replace(/(\d{4})(\d)/, '$1-$2')
+    .replace(/(-\d{2})\d+?$/, '$1');
+};
+
+const phone = (value) => {
+  let r = value.replace(/\D/g, '').replace(/^0/, '');
+  if (r.length > 10) r = r.replace(/^(\d\d)(\d{5})(\d{4}).*/, '($1) $2-$3');
+  else if (r.length > 5) r = r.replace(/^(\d\d)(\d{4})(\d{0,4}).*/, '($1) $2-$3');
+  else if (r.length > 2) r = r.replace(/^(\d\d)(\d{0,5})/, '($1) $2');
+  else r = r.replace(/^(\d*)/, '($1');
+  return r;
+};
+
+export { code, cnpj, phone };
