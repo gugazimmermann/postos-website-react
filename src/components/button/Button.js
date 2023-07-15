3f7af6b643926a7160598a6817558eeb2e762c85
Guga Zimmermann <gugazimmermann@gmail.com>
2023-07-15 16:19:44 -0300

    fix button

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,15 +1,15 @@
 import { Loading } from '../../icons';
 
-const Button = ({ loading, text }) => {
+const Button = ({ type = 'button', loading, text }) => {
   return (
     <button
       disabled={loading}
-      type='button'
+      type={type}
       className={`w-2/3 text-white bg-amber-500 hover:bg-amber-600 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 inline-flex items-center`}
     >
       <div className='text-center w-full'>
         {loading && <Loading />}
-        {text}
+        {!loading ? text : 'Aguarde'}
       </div>
     </button>
   );
