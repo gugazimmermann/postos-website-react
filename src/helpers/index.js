e9f7d64ef117287395c097f4e7ed376a529e4cf5
Guga Zimmermann <gugazimmermann@gmail.com>
2023-07-15 16:25:50 -0300

    br states

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,4 @@
 export { default as isValidCNPJ } from './is-valid-cnpj';
 export { default as isValidEmail } from './is-valid-email';
 export * as masks from './masks';
+export { default as statesBR } from './states-br';
