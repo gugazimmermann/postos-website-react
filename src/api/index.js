0b9d71d69d5aca20e8a610c4d8343654ab36f854
Guga Zimmermann <gugazimmermann@gmail.com>
2023-07-16 00:11:07 -0300

    add fake register calls

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,11 @@ const UseTimeout = (delay) => {
   return new Promise((resolve) => setTimeout(resolve, delay));
 };
 
+const dataId = 'a78bbda1-eb40-452e-be6b-e542c0fb33ba';
+const dataEmail = 'gugazimmermann@gmail.com';
+const dataName = 'Fernandinho';
+const dataCode = 'fernandinho';
+
 const simulateAPICall = async (data) => {
   try {
     await UseTimeout(2000);
@@ -14,18 +19,33 @@ const simulateAPICall = async (data) => {
 };
 
 const postRegister = async ({ document, name, email, phone, address, city, state }) => {
-  const { data } = await simulateAPICall({ id: 'a78bbda1-eb40-452e-be6b-e542c0fb33ba' });
+  const { data } = await simulateAPICall({ id: dataId });
   return data;
 };
 
-const getRegister = async (id) => {
-  const { data } = await simulateAPICall({ email: 'gugazimmermann@gmail.com' });
+const getRegisterCode = async (id) => {
+  const { data } = await simulateAPICall({ id: dataId, email: dataEmail });
   return data;
 };
 
 const postRegisterCode = async ({ id, code }) => {
-  const { data } = await simulateAPICall({ id: 'a78bbda1-eb40-452e-be6b-e542c0fb33ba' });
+  const { data } = await simulateAPICall({ id: dataId });
   return data;
 };
 
-export { postRegister, getRegister, postRegisterCode };
+const getRegisterData = async (id) => {
+  const { data } = await simulateAPICall({ id: dataId, name: dataName });
+  return data;
+};
+
+const postRegisterData = async ({ code, password }) => {
+  const { data } = await simulateAPICall({ id: dataId, code: dataCode });
+  return data;
+};
+
+const getRegisterSuccess = async (id) => {
+  const { data } = await simulateAPICall({ id: dataId, code: dataCode, name: dataName, email: dataEmail });
+  return data;
+};
+
+export { postRegister, getRegisterCode, postRegisterCode, getRegisterData, postRegisterData, getRegisterSuccess };
