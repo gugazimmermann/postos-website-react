aeeea31fc17aa02eda11cdf89762b4916a7e3ace
Guga Zimmermann <gugazimmermann@gmail.com>
2023-07-16 16:59:49 -0300

    refactor footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,70 +1,165 @@
-import { Facebook, Instagram, Threads, Twitter, Youtube, Linkedin } from '../../icons';
+import { Link } from 'react-router-dom';
+import { Whatsapp, Facebook, Instagram, Threads, Twitter, Youtube, Linkedin } from '../../icons';
+import Logo from '../logo/Logo';
 
-const Footer = () => {
-  const Col = ({ children }) => <div className='p-5 w-1/2 sm:w-4/12 md:w-3/12'>{children}</div>;
-
-  const ColTitle = ({ text }) => (
-    <div className='text-sm uppercase text-slate-50 font-bold mb-4'>{text}</div>
-  );
-
-  const ColItem = ({ text }) => (
-    <div className='my-3 block text-slate-50 hover:text-slate-800 hover:font-bold hover:cursor-pointer text-sm'>
-      {text}
+const MainCol = () => {
+  return (
+    <div className='w-full'>
+      <div className='flex flex-col md:flex-row justify-between items-center p-4'>
+        <Logo />
+        <p className='text-slate-500 mt-2 md:mt-0'>
+          Sistema para Gerenciamento de Frotas para Postos de Combustíveis
+        </p>
+      </div>
+      <hr className='border-slate-200' />
     </div>
   );
+};
 
-  const SocialItem = ({ midia }) => {
+const ColumnTitle = ({ title }) => (
+  <h3 className='text-gray-800 text-sm uppercase font-bold'>{title}</h3>
+);
+
+const Column = ({ title, items }) => {
+  return (
+    <div>
+      <ColumnTitle title={title} />
+      {items.map((item) => (
+        <Link
+          key={item.name}
+          to={item.link}
+          className='block mt-2 text-sm text-gray-500 hover:underline hover:cursor-pointer'
+        >
+          {item.name}
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+const ContactColumn = () => {
+  return (
+    <div>
+      <ColumnTitle title='Contato' />
+      <a
+        href='https://wa.me/5547988704247'
+        className='block mt-2 text-sm text-gray-500 hover:underline hover:cursor-pointer'
+      >
+        WhatsApp
+      </a>
+      <a href='tel:+5547988704247' className='block mt-2 text-sm text-gray-500'>
+        (47) 98870-4247
+      </a>
+      <a
+        href='mailto:contato@touchsistemas.com.br'
+        className='block mt-2 text-sm text-gray-500 hover:underline hover:cursor-pointer'
+      >
+        contato@touchsistemas.com.br
+      </a>
+    </div>
+  );
+};
+
+const SocialRow = () => {
+  const SocialIcon = ({ name }) => {
     let icon = null;
-    if (midia === 'facebook') icon = <Facebook />;
-    if (midia === 'instagram') icon = <Instagram />;
-    if (midia === 'threads') icon = <Threads />;
-    if (midia === 'twitter') icon = <Twitter />;
-    if (midia === 'youtube') icon = <Youtube />;
-    if (midia === 'linkedin') icon = <Linkedin />;
+    if (name === 'Whatsapp') icon = <Whatsapp />;
+    if (name === 'Facebook') icon = <Facebook />;
+    if (name === 'Instagram') icon = <Instagram />;
+    if (name === 'Threads') icon = <Threads />;
+    if (name === 'Twitter') icon = <Twitter />;
+    if (name === 'Youtube') icon = <Youtube />;
+    if (name === 'Linkedin') icon = <Linkedin />;
     if (icon) {
-      return <div className='text-slate-50 hover:text-slate-900 hover:cursor-pointer'>{icon}</div>;
+      return (
+        <div className='mx-2 text-slate-500 hover:text-slate-800 hover:cursor-pointer'>{icon}</div>
+      );
     }
     return null;
   };
 
+  const SocialItem = ({ name, linkTo }) => {
+    return (
+      <a href={linkTo} target='_blank' rel='noopener noreferrer' aria-label={name}>
+        <SocialIcon name={name} />
+      </a>
+    );
+  };
+
   return (
-    <footer className='bg-amber-500 sm:mt-4 pt-4 w-full'>
-      <div className='container m-auto flex flex-wrap justify-center text-center'>
-        <Col>
-          <ColTitle text='Sobre Nós' />
-          <ColItem text='A Empresa' />
-          <ColItem text='Área de Atuação' />
-        </Col>
-        <Col>
-          <ColTitle text='Informações' />
-          <ColItem text='Para a Organização' />
-          <ColItem text='Para os Postos' />
-          <ColItem text='Para o Cliente' />
-          <ColItem text='Termos de Uso' />
-        </Col>
-        <Col>
-          <ColTitle text={process.env.REACT_APP_TITLE} />
-          <ColItem text='Atualizações' />
-          <ColItem text='Solicitar Mudanças' />
-          <ColItem text='Novos Relatórios' />
-          <ColItem text='Personalizações' />
-        </Col>
-        <Col>
-          <ColTitle text='Ajuda' />
-          <ColItem text='FAQ' />
-          <ColItem text='Biblioteca' />
-          <ColItem text='Suporte' />
-        </Col>
-      </div>
-      <div className='pt-2'>
-        <div className='flex pb-5 px-3 pt-5 border-t border-slate-50 justify-center gap-4'>
-          <SocialItem midia='facebook' />
-          <SocialItem midia='instagram' />
-          <SocialItem midia='threads' />
-          <SocialItem midia='twitter' />
-          <SocialItem midia='youtube' />
-          <SocialItem midia='linkedin' />
+    <div className='flex mt-6 justify-center'>
+      <SocialItem name='Whatsapp' linkTo='https://wa.me/5547988704247' />
+      <SocialItem name='Facebook' linkTo='https://www.facebook.com/' />
+      <SocialItem name='Instagram' linkTo='https://www.instagram.com/' />
+      <SocialItem name='Threads' linkTo='https://www.threads.net/' />
+      <SocialItem name='Twitter' linkTo='https://www.twitter.com/' />
+      <SocialItem name='Youtube' linkTo='https://www.youtube.com/' />
+      <SocialItem name='Linkedin' linkTo='https://www.linkedin.com/' />
+    </div>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer className='bg-white'>
+      <div className='container mx-auto mb-4'>
+        <MainCol />
+        <div className='flex flex-wrap justify-around mt-4'>
+          <Column
+            title='Sobre Nós'
+            items={[
+              {
+                name: 'A Empresa',
+                link: '/',
+              },
+              {
+                name: 'Trabalhe Conosco',
+                link: '/',
+              },
+            ]}
+          />
+          <Column
+            title='Informações'
+            items={[
+              {
+                name: 'Termos de Uso',
+                link: '/',
+              },
+              {
+                name: 'Privacidade',
+                link: '/',
+              },
+            ]}
+          />
+          <Column
+            title='O Sistema'
+            items={[
+              {
+                name: 'Personalizações',
+                link: '/',
+              },
+              {
+                name: 'Novos Relatórios',
+                link: '/',
+              },
+            ]}
+          />
+          <Column
+            title='Ajuda'
+            items={[
+              {
+                name: 'FAQ',
+                link: '/',
+              },
+              {
+                name: 'Suporte',
+                link: '/',
+              },
+            ]}
+          />
+          <ContactColumn />
         </div>
+        <SocialRow />
       </div>
     </footer>
   );
