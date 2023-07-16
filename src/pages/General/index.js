e4649a837d42e1aaaef133f288b7ffacc63766dd
Guga Zimmermann <gugazimmermann@gmail.com>
2023-07-16 15:44:17 -0300

    gas station page

--- src/pages/General/index.js
+++ src/pages/General/index.js
@@ -1,2 +1,4 @@
 export { default as Page } from './Page';
 export { default as Section } from './Section';
+export { default as SectionTitle } from './SectionTitle';
+export { default as SectionParagraph } from './SectionParagraph';
--- src/pages/gas-stations/GasStations.js
+++ src/pages/gas-stations/GasStations.js
@@ -1,3 +1,4 @@
+import { SectionParagraph, SectionTitle } from '../General';
 import Page from '../General/Page';
 import Section from '../General/Section';
 
@@ -5,14 +6,12 @@ const GasStations = () => {
   return (
     <Page>
       <Section>
-        <h2 className='text-2xl font-semibold text-slate-800  md:text-3xl'>
-          Para os <span className='text-amber-500'>Postos</span>
-        </h2>
-        <p className='mt-4 text-slate-500'>
+        <SectionTitle title='Para os' word='Postos' />
+        <SectionParagraph>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem modi reprehenderit vitae
           exercitationem aliquid dolores ullam temporibus enim expedita aperiam mollitia iure
           consectetur dicta tenetur, porro consequuntur saepe accusantium consequatur.
-        </p>
+        </SectionParagraph>
       </Section>
     </Page>
   );
