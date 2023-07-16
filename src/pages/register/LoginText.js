883c9c18d7528c1e596c6df58b3e6733042d5025
Guga Zimmermann <gugazimmermann@gmail.com>
2023-07-16 14:39:39 -0300

    add info modal

--- src/pages/register/LoginText.js
+++ src/pages/register/LoginText.js
@@ -1,6 +1,22 @@
+import { useModal } from '../../hooks';
 import { Loading } from '../../icons';
+import { ModalOpen } from '../../icons';
 
 const OrganizationDataText = ({ name }) => {
+  const { openModal, Modal } = useModal({ closeoutside: true });
+
+  const MoreInfoButton = () => {
+    return (
+      <button
+        onClick={openModal}
+        className='text-amber-500 hover:underline hover:cursor-pointer text-lg justify-center md:justify-start inline-flex items-center gap-2'
+      >
+        Mais Informações
+        <ModalOpen />
+      </button>
+    );
+  };
+
   const nameText = (name) => {
     if (name) {
       return <span className='font-bold'>{name}</span>;
@@ -10,10 +26,21 @@ const OrganizationDataText = ({ name }) => {
 
   return (
     <div className='flex flex-col justify-center text-center md:text-left gap-4 w-full md:w-1/3'>
-      <h1 className='text-3xl font-bold text-slate-800'>Dados de Acesso</h1>
+      <h1 className='text-3xl font-bold text-slate-800'>Cadastro no Sistema</h1>
       <p className='text-lg text-slate-500 inline'>
-       {nameText(name)}, estamos quase lá, para finalizar complete os dados de acesso.
+        {nameText(name)}, estamos quase lá, para finalizar complete os dados de acesso.
       </p>
+      <MoreInfoButton />
+      <Modal title='Mais Informações'>
+        <div className='flex flex-col justify-center leading-6'>
+          <p className='text-slate-800 mb-2'>
+            <strong>Código da Organização</strong> é um código único que identica sua Organização para acessar o sistema.
+          </p>
+          <p className='text-slate-800 mb-2'>
+            <strong>Usuário</strong> é o que identifica quem está acessando o sistema.
+          </p>
+        </div>
+      </Modal>
     </div>
   );
 };
